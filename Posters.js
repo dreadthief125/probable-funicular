624a07319d26b3c6ec86a5e20051543a728a530d
dreadthief125 <88696805+dreadthief125@users.noreply.github.com>
2023-12-04 16:02:17 -0800

    Add files via upload

---
+++ Posters.js
@@ -0,0 +1,168 @@
+AFRAME.registerComponent("comics-posters", {
+  init: function() {
+    this.postersContainer = this.el;
+    this.posters();
+  },
+
+  posters: function() {
+    const postersRef = [
+      {
+        id: "sun",
+        url: "https://cdn.glitch.com/850dd813-e5e9-4dd0-9751-38981ae74172%2Fsol.jpg?v=1604296185621",
+        x:0,
+        y:0,
+        z:0,
+        
+
+        
+      },
+      {
+        id: "mercury",
+        url: "https://cdn.glitch.com/850dd813-e5e9-4dd0-9751-38981ae74172%2Fmercurio.jpg?v=1604298142259",
+        x:1,
+        y:0,
+        z:-5,
+        property:'rotation',
+        to: '0 360 0',
+        easing:'linear', 
+        loop: true, 
+        dur: 20000,
+        
+
+      },
+
+      {
+        id: "venus",
+        url: "https://cdn.glitch.com/850dd813-e5e9-4dd0-9751-38981ae74172%2Fvenus.jpg?v=1604312437571",
+        x:6,
+        y:0,
+        z:-5,
+        property:'rotation',
+        to: {x:0,y: 360,z: 0},
+        easing:'linear', 
+        loop: true, 
+        dur: 25000
+      },
+      {
+        id: "earth",
+        url: "https://cdn.glitch.com/850dd813-e5e9-4dd0-9751-38981ae74172%2Fearth.jpg?v=1604312404511",
+        x:10.5,
+        y:0,
+        z:0,
+        property:'rotation',
+        to: {x:0,y: 360,z: 0},
+        easing:'linear', 
+        loop: true, 
+        dur: 10000
+      },
+      {
+        id: "mars",
+        url: "https://cdn.glitch.com/850dd813-e5e9-4dd0-9751-38981ae74172%2Fmercurio.jpg?v=1604298142259",
+        x:14.5,
+        y:0,
+        z:0,
+        property:'rotation',
+        to: {x:0,y: 360,z: 0},
+        easing:'linear', 
+        loop: true, 
+        dur: 20000
+      },
+      {
+        id: "jupiter",
+        url: "https://cdn.glitch.com/850dd813-e5e9-4dd0-9751-38981ae74172%2Fjupiter.jpg?v=1604312404561",
+        x:21,
+        y:0,
+        z:-5,
+        property:'rotation',
+        to: {x:0,y: 360,z: 0},
+        easing:'linear', 
+        loop: true, 
+        dur: 40000
+      },
+
+      {
+        id: "saturn",
+        url: "https://cdn.glitch.com/850dd813-e5e9-4dd0-9751-38981ae74172%2Fsaturn.png?v=1604312433927",
+        x:26,
+        y:0,
+        z:-5,
+        property:'rotation',
+        to: {x:0,y: 360,z: 0},
+        easing:'linear', 
+        loop: true, 
+        dur: 4500
+      },
+      {
+        id: "uranus",
+        url: "https://cdn.glitch.com/850dd813-e5e9-4dd0-9751-38981ae74172%2Fmercurio.jpg?v=1604298142259"
+        ,x:31,
+        y:0,
+        z:-5,
+        property:'rotation',
+        to: {x:0,y: 360,z: 0},
+        easing:'linear', 
+        loop: true, 
+        dur: 50000
+      },
+      {
+        id:"neptune",
+        url:"https://cdn.glitch.com/850dd813-e5e9-4dd0-9751-38981ae74172%2Fmercurio.jpg?v=1604298142259"
+        ,x:36,
+        y:0,
+        z:-5,
+        property:'rotation',
+        to: {x:0,y: 360,z: 0},
+        easing:'linear', 
+        loop: true, 
+        dur: 55000
+      }
+    ];
+    let prevoiusXPosition = -100;
+
+    for (var item of postersRef) {
+      const posX = prevoiusXPosition + 20;
+      const posY = 10;
+      const posZ = -40;
+      const position = { x: posX, y: posY, z: posZ };
+      prevoiusXPosition = posX;
+
+      // Border Element
+      const borderEl = this.createBorder(position, item.id);
+
+      // Poster Element
+      const poster = this.createPoster(item);
+      borderEl.appendChild(poster);
+
+      this.postersContainer.appendChild(borderEl);
+    }
+  },
+  createBorder: function(position, item) {
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("id", item.id);
+    entityEl.setAttribute("visible", true);
+    entityEl.setAttribute("geometry", {
+      primitive: "sphere",
+      radius:0.1
+    });
+
+    entityEl.setAttribute("position", { x: item.x, y: item.y, z: item.z });
+    entityEl.setAttribute("material", { color: "#fff" });
+    entityEl.setAttribute("cursor-listener", {});
+    return entityEl;
+  },
+  createPoster: function(item) {
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("visible", true);
+    entityEl.setAttribute("geometry", {
+      primitive: "sphere",
+      radius:1
+    });
+
+    entityEl.setAttribute("position", { x: item.x, y: item.y, z: item.z });
+        
+        entityEl.setAttribute("animation", {property:item.property,to:'0 360 0',easing:'linear',loop:true,dur:item.dur})
+    entityEl.setAttribute("material", { src: item.url });
+
+    return entityEl;
+  }
+});
